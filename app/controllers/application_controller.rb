454f02144dc729d9c00ee24fabb30a990255830b
Mariusz <mariusz@Mariusz-ubuntu>
2013-07-22 11:42:44 +0200

    Skonczylem rozdzial 6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
 	helper_method :current_user
+
+	def load_order
+		begin
+			@order = Order.find(session[:order_id])
+		rescue ActiveRecord::RecordNotFound
+			if current_user
+				@order = current_user.orders.find_or_create_by_status(status: "unsubmitted")
+			else
+				@order = Order.create(status: "unsubmitted")
+			end
+			session[:order_id] = @order.id
+		end
+	end
+
 
 	private
 		def current_user
